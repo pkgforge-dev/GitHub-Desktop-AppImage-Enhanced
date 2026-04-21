adeb8fab6c367c47aa6e7085d437560a6f4de773
Link4Electronics <link4electronics@gmail.com>
2026-04-21 12:10:54 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/github-desktop
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
